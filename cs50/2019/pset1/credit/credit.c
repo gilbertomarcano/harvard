8a7025f37d278368fb34f56115ce6b8d21a5593c
Gilberto Marcano <45245666+gilbertomarcano@users.noreply.github.com>
2020-05-07 00:15:51 -0400

    Update credit.c

--- cs50/2019/pset1/credit/credit.c
+++ cs50/2019/pset1/credit/credit.c
@@ -1,28 +1,3 @@
-/*
-
-In a file called credit.c in a ~/pset1/credit/ directory, write a program that
-prompts the user for a credit card number and then reports (via printf) whether
-it is a valid American Express, MasterCard, or Visa card number, per the
-definitions of each’s format herein. So that we can automate some tests of your
-code, we ask that your program’s last line of output be AMEX\n or MASTERCARD\n
-or VISA\n or INVALID\n, nothing more, nothing less. For simplicity, you may
-assume that the user’s input will be entirely numeric (i.e., devoid of hyphens,
-as might be printed on an actual card). But do not assume that the user’s input
-will fit in an int!
-
-Here are a few card numbers that PayPal recommends for testing.
-American Express	378282246310005
-American Express	371449635398431
-MasterCard	        2221000000000009
-MasterCard	        2223000048400011
-MasterCard	        2223016768739313
-MasterCard	        5555555555554444
-MasterCard	        5105105105105100
-Visa	            4111111111111111
-Visa	            4012888888881881
-Visa	            4222222222222
-*/
-
 #include <stdio.h>
 
 #include "../../cs50.h"
