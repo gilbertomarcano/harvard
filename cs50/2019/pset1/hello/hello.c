a19372e0d812a71e95effa96fe314db81915fb09
Gilberto Marcano <45245666+gilbertomarcano@users.noreply.github.com>
2020-05-06 23:57:45 -0400

    Update hello.c

--- cs50/2019/pset1/hello/hello.c
+++ cs50/2019/pset1/hello/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+#include "../../cs50.h"
+
 int main(void)
 {
     printf("hello, world\n");
+    char* name = get_string("what is your name?\n");
+    printf("hello, %s\n", name);
 }
